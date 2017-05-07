7d6f8ff73ccc054a1d05e023eacce877078b3181
wiki-fan <uberslowpoke@gmail.com>
2017-05-07 21:41:06 +0300

    lab2 finished

--- lab2/lab2.fs
+++ lab2/lab2.fs
@@ -8,7 +8,7 @@ open System.Diagnostics
 open System.Text.RegularExpressions
 
 // почтовый адрес
-let email = ""
+let email = "sharuev.ds@phystech.edu"
 
 
 type NumberType = Int of int|Float of float
@@ -16,10 +16,7 @@ type Token = OpenBrace|CloseBrace|OpenBracket|CloseBracket|Colon|Comma|Null|True
 type JSON = Null|Boolean of bool|String of string|Number of NumberType|Array of JSON list|Object of (string*JSON) list
 
 let explode (s:string) = [for i in s -> i]
-let implode (xs:char list) =
-  let sb = System.Text.StringBuilder(xs.Length)
-  xs |> List.iter (sb.Append >> ignore)
-  sb.ToString()
+let implode (cl:char list) = List.fold (fun acc c -> acc+string(c)) "" cl
 
 let tokenize (source:char list) : Token list = 
   let rec parseString (acc:String) (l:char list) : String*char list = 
@@ -29,7 +26,7 @@ let tokenize (source:char list) : Token list =
       | '\\'::'\\'::t -> parseString (acc+"\\") t
       | '\\'::'/'::t -> parseString (acc+"/") t
       | '\\'::'b'::t -> parseString (acc+"\b") t
-      | '\\'::'f'::t -> parseString (acc+string(char 12)) t
+      | '\\'::'f'::t -> parseString (acc+"\u000C") t
       | '\\'::'n'::t -> parseString (acc+"\n") t
       | '\\'::'r'::t -> parseString (acc+"\r") t
       | '\\'::'t'::t -> parseString (acc+"\t") t
@@ -44,7 +41,6 @@ let tokenize (source:char list) : Token list =
       | t -> (acc, t)
 
   let rec tokenize' (acc:Token list) (str:char list) : Token list = 
-    //Printf.printfn "%A" str
     match str with
       | [] -> acc
       | w::t when Char.IsWhiteSpace(w) -> tokenize' acc t
@@ -75,7 +71,6 @@ let parse source =
   let rec parse' json = 
     // obj - аккумулятор для текущего объекта
     let rec parseObject (obj:(string*JSON) list) json = 
-      //Printf.printfn "%A" json
       match json with
       | Token.String name::Token.Colon::ts -> let (parsed, ts) = parse' ts 
                                               parseObject ((name, parsed)::obj) ts
@@ -89,7 +84,6 @@ let parse source =
       | t -> let (parsed, t) = parse' t
              parseArray (parsed::arr) t
       
-    //Printf.printfn "%A" json
     match json with 
       | Token.Null::t -> JSON.Null, t
       | Token.OpenBrace::t -> parseObject [] t
@@ -108,7 +102,6 @@ let parse source =
 let tree (s:String) = s |> explode |> tokenize |> parse
 
 let example1 = """{"a":1, "b":2}"""
-let res1 = tree example1
 let example2 = 
   """
   {
@@ -122,8 +115,6 @@ let example2 =
     "escapes":["\"", "\\", "\/", "\n", "\r", "\t"]
   } 
   """
-let res2 = tree example2
-
 let example3 = 
   """
   {
@@ -149,24 +140,6 @@ let example3 =
       }
   }
   """
-let res3 = tree example3
-
-let rec lab3 (node:JSON) : JSON =
-  match node with
-    | JSON.Object o -> 
-      JSON.Object <| List.map (fun ((key, value) : string*JSON) -> 
-        match value with
-          | JSON.String value -> (value, JSON.String(key)) 
-          | _ -> (string(key), lab3 value)
-      ) o
-    | JSON.Array a ->
-      JSON.Array <| List.map (lab3) a
-    | o -> o
-
-lab3(tree(example1))
-lab3(tree(example2))
-lab3(tree(example3))
-
 let example4 =
   """
   {
@@ -175,15 +148,92 @@ let example4 =
     "c":"d"
   }
   """
-lab3(tree(example4))
-let stringify = function
-  | Object list -> "{}"
 
-let generate = 
+let res1 = tree example1
+let res2 = tree example2
+let res3 = tree example3
+let res4 = tree example4
+
+// Ключ может быть только строкой, а значение — чем угодно. Получается, сделать требуемое нужно только для пар вида str1:str2.
+// (вариант, в котором мы рекурсивно заходим в поддеревья, обрабатываем их, а потом при помощи stringify делаем из значения строку и таки меняем
+// ключ и значение местами, очень странно выглядит)
+let rec lab2 (node:JSON) : JSON =
+  match node with
+    | JSON.Object o -> 
+      JSON.Object <| List.map (fun ((key, value) : string*JSON) -> 
+        match value with
+          | JSON.String value -> (value, JSON.String(key)) 
+          | _ -> (string(key), lab2 value)
+      ) o
+    | JSON.Array a ->
+      JSON.Array <| List.map (lab2) a
+    | o -> o
+
+lab2(tree(example1))
+lab2(tree(example2))
+lab2(tree(example3))
+lab2(tree(example4))
+
+let rec stringify (json:JSON):string = 
+  let tab = "\t"
+  let escape (s:string):string = 
+    let escape' (c:char) : char list = 
+      match c with
+      | '\"' -> ['\\';'\"']
+      | '\\' -> ['\\';'\\']
+      | '/' -> ['\\';'/']
+      | '\b' -> ['\\';'b']
+      | '\u000C' -> ['\\';'f']
+      | '\n' -> ['\\';'n']
+      | '\r' -> ['\\';'r']
+      | '\t' -> ['\\';'t']
+      | c -> [c]
+    
+    implode(List.concat (List.map escape' (explode s)))
+
+  let wrap s = 
+    "\""+(escape s)+"\""
+  match json with
+  | JSON.Null -> "null"
+  | JSON.Boolean b -> if b then "true" else "false"
+  | JSON.Number(NumberType.Float f)-> string(f)
+  | JSON.Number(NumberType.Int n) -> string(n)
+  | JSON.String s -> wrap s
+  | JSON.Array a -> "["+(String.concat ", " (List.map stringify a))+"]"
+  | JSON.Object o -> "{"+(String.concat ", " (List.map (fun field -> (wrap (fst field)) + " : " + (stringify (snd field))) o))+"}" 
+
+stringify res1
+stringify res2
+stringify res3
+stringify res4
+
+let generate () : JSON = 
+  let MAX_STRING_LENGTH = 10
+  let MAX_KEY_LENGTH = 10
+  let MAX_OBJECT_LEN = 10
+  let MAX_ARRAY_LEN = 10
+  let MAX_DEPTH = 10
   let rnd = new Random()
-  match rnd.Next(42) with
-    | 0 -> Object []
-    | _ -> Object [("random", Object [])]
+
+  let getRandChar() = char((int 'a')+(rnd.Next(int 'z' - int 'a')))
+  let getRandString length : string = List.fold (fun acc c -> acc+(string c)) "" ([for i in 0..length -> getRandChar()])
+
+  let rec generate' (depth:int) = 
+    if depth=0 then JSON.Null else 
+      match rnd.Next 7 with 
+        | 0 -> JSON.Null
+        | 1 -> JSON.Boolean ((rnd.Next 2) = 0)
+        | 2 -> JSON.Number(NumberType.Int(rnd.Next()))
+        | 3 -> JSON.Number(NumberType.Float(rnd.NextDouble()))
+        | 4 -> JSON.String(getRandString (rnd.Next MAX_STRING_LENGTH))
+        | 5 -> JSON.Object([for i in 0..(rnd.Next MAX_OBJECT_LEN) -> (getRandString (rnd.Next MAX_KEY_LENGTH), generate' (depth-1)) ])
+        | 6 -> JSON.Array([for i in 0..(rnd.Next MAX_ARRAY_LEN) -> generate' (depth-1)])
+        | _ -> failwith "Impossible match"
+  generate' MAX_DEPTH
+
+let randtree = generate()
+printf "%s" (String.Concat [(stringify (randtree)); "\n"; (stringify (lab2(randtree)))])
+(stringify (randtree)) = (stringify (lab2(lab2(randtree))))
 
 let main () = 
   let values = new NameValueCollection()
@@ -195,3 +245,5 @@ let main () =
   let responseString = Text.Encoding.Default.GetString(response)
 
   printf "%A\n" responseString
+
+main()
